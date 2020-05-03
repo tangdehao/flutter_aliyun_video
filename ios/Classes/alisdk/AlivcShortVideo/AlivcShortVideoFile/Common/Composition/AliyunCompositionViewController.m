ed5a8035d693c1168bf77f98e014eae6b1b1b564
李鹏达 <love-lpd@163.com>
2020-05-04 00:20:46 +0800

    color

--- ios/Classes/alisdk/AlivcShortVideo/AlivcShortVideoFile/Common/Composition/AliyunCompositionViewController.m
+++ ios/Classes/alisdk/AlivcShortVideo/AlivcShortVideoFile/Common/Composition/AliyunCompositionViewController.m
@@ -235,7 +235,7 @@
     UILabel *tipsLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 64+top+5, ScreenWidth-20, 30)];
     tipsLabel.text = @"Only display videos shorter than 30 seconds";
     tipsLabel.font = [UIFont systemFontOfSize:16];
-    tipsLabel.backgroundColor = [UIColor whiteColor];
+    tipsLabel.textColor = [UIColor whiteColor];
     [self.view addSubview:tipsLabel];
 }
 
